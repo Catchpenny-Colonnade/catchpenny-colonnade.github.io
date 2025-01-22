84db4e483cdff586a0c4ece0f8cf356a337c6f93
Daniel Johnson <voltron42@users.noreply.github.com>
2025-01-22 18:24:28 +0000

    adding file build script

--- sudoku/sudoku-clj/src/sudoku_clj/core.clj
+++ sudoku/sudoku-clj/src/sudoku_clj/core.clj
@@ -1,6 +1,21 @@
-(ns sudoku-clj.core)
+(ns sudoku-clj.core 
+  (:require
+   [clojure.java.io :as io]
+   [clojure.data.json :as json]))
 
 (defn foo
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
+
+(defn -main [& _]
+  (let [indicies (json/read-str (slurp "resources/indicies.json"))
+        index-count (count indicies)]
+    (loop [lines (rest (line-seq (io/reader "resources/sudoku.csv")))
+           index 0]
+      (when-not (empty? lines)
+        (spit
+         (format "resources/puzzles/index%s.json" (nth index indicies))
+         (format "%s\n" (first lines))
+         :append true))
+      (recur (rest lines) (mod (inc index) index-count)))))
