16f09ec9efd5e96d751a334cb566e3bd9463c129
Daniel Johnson <voltron42@users.noreply.github.com>
2025-10-13 00:26:04 -0500

    pyramid init

---
+++ pyramid/pyramid.js
@@ -0,0 +1,110 @@
+namespace("pyramid.PyramidGame", {}, ({}) => {
+    return class extends React.Component {
+        constructor(props) {
+            super(props);
+            this.state = {};
+        }
+        render() {
+            return <div className="d-flex flex-column h-100 mt-5">
+                <div className="d-flex justify-content-center mb-2 mt-2">
+                    <div className="col-1">
+                        <button className="btn btn-danger w-100">{ 'D\u2663' }</button>
+                    </div>
+                </div>
+                <div className="d-flex justify-content-center mb-2 mt-2">
+                    <div className="col-1">
+                        <button className="btn btn-danger w-100">{ 'D\u2663' }</button>
+                    </div>
+                    <div className="col-1">
+                        <button className="btn btn-danger w-100">{ 'D\u2663' }</button>
+                    </div>
+                </div>
+                <div className="d-flex justify-content-center mb-2 mt-2">
+                    <div className="col-1">
+                        <button className="btn btn-danger w-100">{ 'D\u2663' }</button>
+                    </div>
+                    <div className="col-1">
+                        <button className="btn btn-danger w-100">{ 'D\u2663' }</button>
+                    </div>
+                    <div className="col-1">
+                        <button className="btn btn-danger w-100">{ 'D\u2663' }</button>
+                    </div>
+                </div>
+                <div className="d-flex justify-content-center mb-2 mt-2">
+                    <div className="col-1">
+                        <button className="btn btn-danger w-100">{ 'D\u2663' }</button>
+                    </div>
+                    <div className="col-1">
+                        <button className="btn btn-danger w-100">{ 'D\u2663' }</button>
+                    </div>
+                    <div className="col-1">
+                        <button className="btn btn-danger w-100">{ 'D\u2663' }</button>
+                    </div>
+                    <div className="col-1">
+                        <button className="btn btn-danger w-100">{ 'D\u2663' }</button>
+                    </div>
+                </div>
+                <div className="d-flex justify-content-center mb-2 mt-2">
+                    <div className="col-1">
+                        <button className="btn btn-danger w-100">{ 'D\u2663' }</button>
+                    </div>
+                    <div className="col-1">
+                        <button className="btn btn-danger w-100">{ 'D\u2663' }</button>
+                    </div>
+                    <div className="col-1">
+                        <button className="btn btn-danger w-100">{ 'D\u2663' }</button>
+                    </div>
+                    <div className="col-1">
+                        <button className="btn btn-danger w-100">{ 'D\u2663' }</button>
+                    </div>
+                    <div className="col-1">
+                        <button className="btn btn-danger w-100">{ 'D\u2663' }</button>
+                    </div>
+                </div>
+                <div className="d-flex justify-content-center mb-2 mt-2">
+                    <div className="col-1">
+                        <button className="btn btn-danger w-100">{ 'D\u2663' }</button>
+                    </div>
+                    <div className="col-1">
+                        <button className="btn btn-danger w-100">{ 'D\u2663' }</button>
+                    </div>
+                    <div className="col-1">
+                        <button className="btn btn-danger w-100">{ 'D\u2663' }</button>
+                    </div>
+                    <div className="col-1">
+                        <button className="btn btn-danger w-100">{ 'D\u2663' }</button>
+                    </div>
+                    <div className="col-1">
+                        <button className="btn btn-danger w-100">{ 'D\u2663' }</button>
+                    </div>
+                    <div className="col-1">
+                        <button className="btn btn-danger w-100">{ 'D\u2663' }</button>
+                    </div>
+                </div>
+                <div className="d-flex justify-content-center mb-2 mt-2">
+                    <div className="col-1">
+                        <button className="btn btn-danger w-100">{ 'D\u2663' }</button>
+                    </div>
+                    <div className="col-1">
+                        <button className="btn btn-danger w-100">{ 'D\u2663' }</button>
+                    </div>
+                    <div className="col-1">
+                        <button className="btn btn-danger w-100">{ 'D\u2663' }</button>
+                    </div>
+                    <div className="col-1">
+                        <button className="btn btn-danger w-100">{ 'D\u2663' }</button>
+                    </div>
+                    <div className="col-1">
+                        <button className="btn btn-danger w-100">{ 'D\u2663' }</button>
+                    </div>
+                    <div className="col-1">
+                        <button className="btn btn-danger w-100">{ 'D\u2663' }</button>
+                    </div>
+                    <div className="col-1">
+                        <button className="btn btn-danger w-100">{ 'D\u2663' }</button>
+                    </div>
+                </div>
+            </div>;
+        }
+    }
+});
