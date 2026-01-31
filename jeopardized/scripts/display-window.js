2b306c0044d04bdbabf2a8dec727e437973fe346
Daniel Johnson <voltron42@users.noreply.github.com>
2026-01-31 21:53:25 +0000

    building out "DisplayWindow" class

---
+++ jeopardized/scripts/display-window.js
@@ -0,0 +1,26 @@
+namespace("jeopardized.DisplayWindow", {}, () => {
+  const DisplayWindow = function(displayName, windowTitle, rootClass, TemplateClass, addlAttrs) {
+    const updateEvent = `window.${displayName}.update`;
+    const state = {};
+    this.open = function(initState) {
+      state.sidecar = window.open("", "_blank");
+      state.sidecar.document.title = windowTitle;
+      state.sidecar.document.write(`<div id="${displayName}" class="${rootClass}"></div>`);
+      const root = state.sidecar.document.getElementById(displayName);
+      ReactDOM.createRoot(root).render(<TemplateClass 
+        initState={initState}
+        addlAttrs={addlAttrs}
+        setOnUpdate={(setter) => 
+          document.addEventListener(updateEvent, (e) => {
+            setter(e.detail);
+          })}></TemplateClass>)
+    }
+    this.update = function(args) {
+      document.dispatchEvent(updateEvent, { detail: args });
+    }
+    this.close = function() {
+      state.sidecar.close();
+    }
+  };
+  return DisplayWindow;
+});
