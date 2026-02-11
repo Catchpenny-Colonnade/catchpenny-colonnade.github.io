2481032aded0b4326dc9cfce4a8666d8aa3d0fdd
Daniel Johnson <voltron42@users.noreply.github.com>
2026-02-11 01:50:59 +0000

    building out memory

--- memory/scripts/memory.js
+++ memory/scripts/memory.js
@@ -1,8 +1,22 @@
 namespace("memory.Memory", {
   "memory.Icons": "Icons"
 }, ({ Icons }) => {
+  const iconIndexList = "?".repeat(Icons.getIconCount()).split("").map((_,i) => i);
+  const pairCount = 24;
   const shuffle = function() {
-    // todo
+    const allIcons = Array.from(iconIndexList);
+    const deck = [];
+    while(deck.length < pairCount) {
+      deck.push(allIcons.splice(Math.floor(Math.random() * allIcons.length), 1));
+    }
+    const doubleDeck = deck.concat(deck);
+    const shuffled = [];
+    while(doubleDeck.length > 0) {
+      shuffled.push(doubleDeck.splice(Math.floor(Math.random() * doubleDeck.length), 1));
+    }
+    return shuffled.map((iconIndex) => {
+      return { iconIndex, revealed: false };
+    });
   };
   return class extends React.Component {
     constructor(props) {
@@ -12,14 +26,40 @@ namespace("memory.Memory", {
       };
     }
     click(index) {
+      if (isNaN(this.state.first)) {
+        this.setState({ first: index });
+      } else if (isNaN(this.state.second)) {
+        this.setState({ second: index })
+      }
+    }
+    afterRender() {
+      const { first, second, board } = this.state;
+      if (!isNaN(first) && !isNaN(second)) {
+        const [ firstCard, secondCard ] = [ first, second ].map(i => board[i]);
+        if ( firstCard.iconIndex === secondCard.iconIndex ) {
+          alert("Cards match!");
+          firstCard.revealed = true;
+          secondCard.revealed = true
+          this.setState({ board, first: undefined, second: undefined });
+        } else {
+          alert("Cards don't match!");
+          this.setState({ first: undefined, second: undefined });
+        }
+      }
       // todo
+    }
+    componentDidMount() {
+      this.afterRender();
+    }
+    componentDidUpdate() {
+      this.afterRender();
     }
     render() {
       return <>
         <h1 className="text-center">Memory!</h1>
         <div className="row justify-content-center">
           { this.state.board.map(({ iconIndex, revealed }, index) => {
-            var showCard = revealed || index == this.state.first || index
+            var showCard = revealed || index == this.state.first || index == this.state.second;
             return <button className="btn btn-primary icon-frame" onClick={() => this.click(index)}>
               { showCard?Icons.getIconByIndex(iconIndex):Icons.getCardBack() }
             </button>;
